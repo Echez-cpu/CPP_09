86a6e561e6bb1f76fc06c833e077ceab84b56c52
Okpalaeze patrick Echezona <129857369+Echez-cpu@users.noreply.github.com>
2025-03-05 21:46:40 +0100

    Update BitcoinExchange.cpp

--- ex00/srcs/BitcoinExchange.cpp
+++ ex00/srcs/BitcoinExchange.cpp
@@ -67,7 +67,7 @@ void BitcoinExchange::parse_input(std::ifstream &file) {
         this->input_array[i] = 0;
         if (!check_positive(line)) this->input_array[i] = 1;
         if (!check_date(line) || line.length() < 11) this->input_array[i] = 2;
-        if (!_checkTooLarge(line)) this->input_array[i] = 3;
+        if (!check_if_too_Large(line)) this->input_array[i] = 3;
         i++;
     }
 }
