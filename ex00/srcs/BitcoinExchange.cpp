f2fa27edb4dd39db99320c2c9b89ff30482d0d62
Okpalaeze patrick Echezona <129857369+Echez-cpu@users.noreply.github.com>
2025-03-06 00:23:23 +0100

    Update BitcoinExchange.cpp

--- ex00/srcs/BitcoinExchange.cpp
+++ ex00/srcs/BitcoinExchange.cpp
@@ -154,7 +154,7 @@ bool BitcoinExchange::check_if_too_Large(str line) {
 }
 
 
-void BitcoinExchange::_nearestDate(str &key) {
+void BitcoinExchange::nearest_date(str &key) {
     std::map<str, float>::iterator it = _map.lower_bound(key);
     if (it == map_array.begin()) {
         key = "Error: Year too old.";
