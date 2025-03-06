b624aa82377055fa9a6625e4a2d8aa39912194b9
Okpalaeze patrick Echezona <129857369+Echez-cpu@users.noreply.github.com>
2025-03-06 17:06:07 +0100

    Update main.cpp

--- ex00/srcs/main.cpp
+++ ex00/srcs/main.cpp
@@ -13,5 +13,44 @@ str		trim(const str & string)
 
 
 
+void testInput(std::ifstream &file, int argc, char **argv) {
+    if (argc != 2)
+        throw std::runtime_error("Error: Usage: ./btc [filename.txt]");
+    
+    file.open(argv[1], std::ifstream::in);
+    if (!file.is_open())
+        throw std::runtime_error("Error: Could not open file: " + str(av[1]));
+
+    str line;
+    bool isEmpty = true;
+    
+    while (getline(file, line)) {
+        if (!line.empty())
+            isEmpty = false;
+    }
+    
+    if (isEmpty)
+        throw std::runtime_error("Error: Empty line in input file.");
+    
+    file.clear();       
+    file.seekg(0, std::ios::beg);
+}
 
 
+
+int	main(int argc, char **argv) {
+	std::ifstream		file;
+	BitcoinExchange *	exchange;
+	
+	try {
+		testInput(file, argc, argv);
+		exchange = new BitcoinExchange(file, argv);
+	}
+	catch (const std::exception & e) {
+		std::cout << e.what() << std::endl;
+		return 1;
+	}
+
+	delete exchange;
+	return 0;
+}
