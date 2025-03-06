7c8135150fa69920cf4de4caf36244203ca31be6
Okpalaeze patrick Echezona <129857369+Echez-cpu@users.noreply.github.com>
2025-03-06 18:21:06 +0100

    Update main.cpp

--- ex00/srcs/main.cpp
+++ ex00/srcs/main.cpp
@@ -1,18 +1,5 @@
 #include "../includes/BitcoinExchange.hpp"
 
-str		trim(const str & string)
-{
-    size_t first = string.find_first_not_of(' ');
-    if (str::npos == first)
-    {
-        return string;
-    }
-    size_t last = string.find_last_not_of(' ');
-    return string.substr(first, (last - first + 1));
-}
-
-
-
 void testInput(std::ifstream &file, int argc, char **argv) {
     if (argc != 2)
         throw std::runtime_error("Error: Usage: ./btc [filename.txt]");
