e077643bd635577cbdd295dec18b30fd30bd0fb3
Okpalaeze patrick Echezona <129857369+Echez-cpu@users.noreply.github.com>
2025-03-06 18:21:27 +0100

    Update BitcoinExchange.cpp

--- ex00/srcs/BitcoinExchange.cpp
+++ ex00/srcs/BitcoinExchange.cpp
@@ -187,3 +187,16 @@ int BitcoinExchange::get_input_size(std::ifstream &file) const {
 
     return count;
 }
+
+str		trim(const str & string)
+{
+    size_t first = string.find_first_not_of(' ');
+    if (str::npos == first)
+    {
+        return string;
+    }
+    size_t last = string.find_last_not_of(' ');
+    return string.substr(first, (last - first + 1));
+}
+
+
