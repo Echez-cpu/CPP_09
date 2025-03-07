235794eb32e4f52f13ff36369b33926e5f69a718
Okpalaeze patrick Echezona <129857369+Echez-cpu@users.noreply.github.com>
2025-03-07 14:46:10 +0100

    Create RPN.hpp

---
+++ ex01/RPN.hpp
@@ -0,0 +1,26 @@
+#ifndef RPN_HPP
+#define RPN_HPP
+
+#include <iostream>
+#include <stack>
+#include <sstream>
+#include <stdexcept>
+
+typedef std::string str;
+
+class RPN {
+public:
+    RPN();                     
+    RPN(const RPN &source);   
+    RPN &operator=(const RPN &original_copy);
+    ~RPN();
+
+    int evaluate(const str &expression);
+
+private:
+    std::stack<int> stack_obj;
+    bool isOperator(const std::string &token); 
+    int applyOperation(int a, int b, char op); 
+};
+
+#endif
