3b46d53993cb8b7b869ccb986a25d110e8b523b1
Okpalaeze patrick Echezona <129857369+Echez-cpu@users.noreply.github.com>
2025-03-08 19:15:29 +0100

    Create PmergeMe.hpp

---
+++ ex02/includes/PmergeMe.hpp
@@ -0,0 +1,25 @@
+#ifndef PMERGEME_HPP
+#define PMERGEME_HPP
+
+#include <iostream>
+#include <vector>
+#include <deque>
+#include <algorithm>
+#include <sstream>
+#include <ctime>
+
+class PmergeMe {
+public:
+    PmergeMe();
+    PmergeMe(const PmergeMe &source);
+    PmergeMe &operator=(const PmergeMe &original_copy);
+    ~PmergeMe();
+
+    void processInput(char **argv);
+    void sortVector(std::vector<int> &arr);
+    void sortDeque(std::deque<int> &arr);
+    void mergeInsertionSort(std::vector<int> &arr); // FordJohnSon_algo
+    void mergeInsertionSort(std::deque<int> &arr);
+};
+
+#endif
