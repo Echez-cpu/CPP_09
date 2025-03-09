bb7fcf7055440ffc62e3514ff44c228074d53882
Okpalaeze patrick Echezona <129857369+Echez-cpu@users.noreply.github.com>
2025-03-09 14:18:41 +0100

    Update main.cpp

--- ex02/srcs/main.cpp
+++ ex02/srcs/main.cpp
@@ -1,11 +1,16 @@
 #include "../includes/PmergeMe.hpp"
 
 int main(int argc, char **argv) {
-    if (argc < 2) {
+    if (argc == 1) {
         std::cerr << R << "Error: " << RESET << Y << "usage: ./PmergeMe <random ints>" << RESET << std::endl;
         return 1;
     }
 
+if (argc == 2) {
+        std::cerr << R << "Error: " << RESET << Y << "usage: <random ints> must be more than 1 " << RESET << std::endl;
+        return 1;
+    }
+    
     try {
         PmergeMe sorter;
         sorter.processInput(argv);
