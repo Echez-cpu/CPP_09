7d5b01d5aa36aa5082d788abddcf5188c66bb61f
Okpalaeze patrick Echezona <129857369+Echez-cpu@users.noreply.github.com>
2025-03-09 14:21:34 +0100

    Update main.cpp

--- ex02/srcs/main.cpp
+++ ex02/srcs/main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-if (argc == 2) {
+    if (argc == 2) {
         std::cerr << R << "Error: " << RESET << Y << "usage: <random ints> must be more than 1 " << RESET << std::endl;
         return 1;
     }
